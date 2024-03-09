8e01bf3aed06d1a6b6cf658030dd6c7d1868acb4
Michael Ozoudeh <jesmikky@gmail.com>
2024-03-09 18:43:38 +0100

    updated gitinore

--- app/Services/FileUploadServices.php
+++ app/Services/FileUploadServices.php
@@ -12,9 +12,9 @@ class FileUploadServices  implements FileUploadInterface{
     {
         foreach($request->document as $files){
             if($files['docs'] instanceof UploadedFile){
-                $base64Image = base64_encode(file_get_contents($files['docs']->getRealPath()).auth()->user()?->name);
+                $base64Image = base64_encode(file_get_contents($files['docs']->getRealPath()));
             }
-        $documents =    Document::create([
+        $documents =  Document::create([
                 'company_id' => $request->company_id,
                 'document' => $base64Image,
                 'document_type_id' => $files['document_type_id']
