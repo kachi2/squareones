5e193d0a19f885936a430f6de905291b05afa8c0
Michael Ozoudeh <jesmikky@gmail.com>
2024-03-11 00:53:25 +0100

    removed conflicts with staging branch

--- resources/js/Pages/CompanyFormation/Sections/FormSectionCompanySecretary.vue
+++ resources/js/Pages/CompanyFormation/Sections/FormSectionCompanySecretary.vue
@@ -1,155 +1,69 @@
-<script setup>
-import FormContainer from "@/Components/Form/FormContainer.vue";
-import SecondaryButton from "@/Components/SecondaryButton.vue";
-import TextInput from "@/Components/TextInput.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
-import { ArrowLeftIcon, ArrowRightIcon } from "@placetopay/iconsax-vue/twotone";
-import ComboMenu from "@/Components/ComboMenu.vue";
-
-const secretaryTypes = [
-    { id: 1, name: "Corporate" },
-    { id: 2, name: "Customary" },
-];
-</script>
-
 <template>
-    <FormContainer>
-        <div class="mb-6">
-            <h1 class="mb-4 text-2xl font-bold text-gray-700">
-                Company Secretary
-            </h1>
+    <div class="w-3/5 p-5 bg-gray-100">
+        <div class="bg-white p-8 rounded-md shadow-md">
+            <div class="container mx-auto">
+                <div class="mx-auto w-full max-w-xl bg-white p-8">
+                    <h1 class="mb-4 text-2xl font-bold text-gray-700">Company Secretary</h1>
 
-            <div class="mb-6">
-                <label
-                    class="mb-2 block text-sm font-bold text-gray-700"
-                    for="type"
-                >
-                    Company Secretary Details
-                </label>
+                    <div class="mb-6">
+                    <label class="mb-2 block text-sm font-bold text-gray-700" for="type"> Company Secretary Details </label>
+                    <label class="mb-1 block text-sm font-bold text-gray-700" for="type"> Type </label>
+                    <input class="focus:shadow-outline mb-3 w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none" id="type" type="text" placeholder="Corporate" />
 
-                <div class="my-5">
-                    <label
-                        class="mb-1 block text-sm font-bold text-gray-700"
-                        for="type"
-                    >
-                        Type
-                    </label>
-                    <ComboMenu :options="secretaryTypes" />
-                </div>
+                    <label class="mb-1 block text-sm font-bold text-gray-700" for="name-english"> Name in English </label>
+                    <input class="focus:shadow-outline mb-3 w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none" id="name-english" type="text" placeholder="Company name" />
 
-                <div class="my-5">
-                    <label
-                        class="mb-1 block text-sm font-bold text-gray-700"
-                        for="name-english"
-                    >
-                        Name in English
-                    </label>
-                    <TextInput
-                        id="name-english"
-                        type="text"
-                        placeholder="Company name"
-                    />
-                </div>
+                    <label class="mb-1 block text-sm font-bold text-gray-700" for="name-chinese"> Name in Chinese </label>
+                    <input class="focus:shadow-outline mb-3 w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none" id="name-chinese" type="text" placeholder="Company name in Chinese" />
 
-                <div class="my-5">
-                    <label
-                        class="mb-1 block text-sm font-bold text-gray-700"
-                        for="name-chinese"
-                    >
-                        Name in Chinese
-                    </label>
-                    <TextInput
-                        id="name-chinese"
-                        type="text"
-                        placeholder="Company name in Chinese"
-                    />
-                </div>
-
-                <div class="my-5">
-                    <label
-                        class="mb-1 block text-sm font-bold text-gray-700"
-                        for="registration-no"
-                    >
-                        Company registration no.
-                    </label>
-                    <TextInput id="registration-no" type="text" />
-                </div>
-                <div class="my-5">
-                    <label
-                        class="mb-1 block text-sm font-bold text-gray-700"
-                        for="country-registered"
-                    >
-                        Country registered in
-                    </label>
-                    <ComboMenu :options="[]" />
-                </div>
-
-                <label
-                    class="mb-1 block text-sm font-bold text-gray-700"
-                    for="address"
-                >
-                    Address
-                </label>
-                <div class="my-3">
-                    <TextInput id="first" type="text" />
-                </div>
-                <div class="my-3">
-                    <TextInput id="second" type="text" />
-                </div>
-                <div class="my-3">
-                    <TextInput id="third" type="text" />
-                </div>
-                <div class="my-3">
-                    <TextInput id="fourth" type="text" />
-                </div>
-                <div class="my-3">
-                    <ComboMenu :options="[]" />
-                </div>
-
-                <div class="my-5">
-                    <label
-                        class="mb-1 block text-sm font-bold text-gray-700"
-                        for="license-no"
-                    >
-                        License No.
-                    </label>
-                    <TextInput id="license-no" type="text" />
-                </div>
-
-                <div class="my-5">
-                    <label
-                        class="mb-1 block text-sm font-bold text-gray-700"
-                        for="email"
-                    >
-                        Email
-                    </label>
-                    <TextInput id="email" type="email" />
-                </div>
-            </div>
-
-            <div class="flex space-x-3">
-                <SecondaryButton>
-                    <div class="flex space-x-2">
-                        <ArrowLeftIcon class="text-black h-5 w-5 my-auto" />
-                        <div>Back</div>
+                    <div class="mb-3 flex justify-between">
+                        <div class="mr-2 w-1/2">
+                        <label class="mb-1 block text-sm font-bold text-gray-700" for="registration-no"> Company registration no. </label>
+                        <input class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none" id="registration-no" type="text" />
+                        </div>
+                        <div class="ml-2 w-1/2">
+                        <label class="mb-1 block text-sm font-bold text-gray-700" for="country-registered"> Country registered in </label>
+                        <div class="relative">
+                            <select class="focus:shadow-outline block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none" id="country-registered">
+                            <!-- Insert country options here -->
+                            </select>
+                            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <!-- SVG for the dropdown arrow -->
+                            </div>
+                        </div>
+                        </div>
                     </div>
-                </SecondaryButton>
-                <PrimaryButton>
-                    <div class="flex space-x-2">
-                        <div>Save and Continue</div>
-                        <ArrowRightIcon class="text-white h-5 w-5 my-auto" />
+
+                    <label class="mb-1 block text-sm font-bold text-gray-700" for="address"> Address </label>
+                    <div class="relative">
+                        <select class="focus:shadow-outline block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none" id="address">
+                            <option></option>
+                        </select>
                     </div>
-                </PrimaryButton>
-                <SecondaryButton>
-                    <div class="flex space-x-2">
-                        <ArrowRightIcon class="text-black h-5 w-5 my-auto" />
-                        <div>Skip</div>
+
+                    <label class="mb-1 block text-sm font-bold text-gray-700" for="license-no"> License No. </label>
+                    <input class="focus:shadow-outline mb-3 w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none" id="license-no" type="text" />
+
+                    <label class="mb-1 block text-sm font-bold text-gray-700" for="email"> Email </label>
+                    <input class="focus:shadow-outline mb-3 w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none" id="email" type="email" />
                     </div>
-                </SecondaryButton>
+
+                    <div class="mt-6 flex items-center justify-between">
+                    <button class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+                        <span>Back</span>
+                    </button>
+                    <button class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+                        <span>Continue</span>
+                    </button>
+                    </div>
+                </div>
             </div>
         </div>
-    </FormContainer>
-    <div class="w-2/5 p-5 text-sm" style="background: #f6f9fb">
-        <div class="h-full p-5"></div>
+        
+    </div>    
+    <div class="w-2/5 p-5 text-sm" style="background: #F6F9FB">
+        <div class="h-full p-5">
+            
+        </div> 
     </div>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Users\HomeController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -20,8 +19,8 @@ Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        // 'laravelVersion' => Application::VERSION,
-        // 'phpVersion' => PHP_VERSION,
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
 });
 
@@ -34,7 +33,7 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
     Route::get('/welcome', function() {
-        return Inertia::render('Test/form');
+        return Inertia::render('Welcome');
     })->name('welcome');
     Route::get('/companyformation', function() {
         return Inertia::render('CompanyFormation/FormationPage');
