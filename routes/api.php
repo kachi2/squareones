9ad8fe97dd6f19349401e6d39c73ac1978043959
Michael Ozoudeh <jesmikky@gmail.com>
2024-03-13 17:24:50 +0100

    completed user authentication

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,7 @@
 <?php
 
-use App\Http\Controllers\CompanyActivityController;
-use App\Http\Controllers\CompanyController;
-use App\Http\Controllers\CompanyEntityController;
-use App\Http\Controllers\FileUploadController;
-use App\Http\Controllers\FounderController;
-use App\Http\Controllers\CompanySharesController;
-use App\Http\Controllers\FundSourceController;
-use App\Http\Controllers\SecretaryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
