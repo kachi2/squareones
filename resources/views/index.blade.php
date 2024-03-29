a70bfc28be47496c4e59731de4c436914851d8e1
Michael Ozoudeh <jesmikky@gmail.com>
2024-03-30 00:04:40 +0100

    company formation input validaiton

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <link rel="icon" href="/favicon.ico">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Squareone</title>
-    <script type="module" crossorigin src="/assets/index-BeIEILmY.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-DPOCI9wJ.css">
-  </head>
-  
-  <body>
-    <div id="app"></div>
-  </body>
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="icon" href="/favicon.ico">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Squareone</title>
+  <script type="module" crossorigin src="/assets/index-CFRvuIeA.js"></script>
+  <link rel="stylesheet" crossorigin href="/assets/index-DPOCI9wJ.css">
+</head>
+
+<body>
+  <div id="app"></div>
+</body>
 
 </html>
