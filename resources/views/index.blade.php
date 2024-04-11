e735f2d04d4be03585993802bc89b302ad5124ee
Michael Ozoudeh <jesmikky@gmail.com>
2024-04-11 22:54:37 +0100

    push the new build ap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
   <link rel="icon" href="/favicon.ico">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Squareone</title>
-  <script type="module" crossorigin src="/assets/index-BAeyKVRT.js"></script>
+  <script type="module" crossorigin src="/assets/index-CK-ooKFv.js"></script>
   <link rel="stylesheet" crossorigin href="/assets/index-DK7vx5Vc.css">
 </head>
 
