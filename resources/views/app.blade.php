641c8bf2e2caed2e2daa6072374682ce140a620f
Michael Ozoudeh <jesmikky@gmail.com>
2024-04-12 00:35:05 +0100

    added docs

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
   <link rel="icon" href="/favicon.ico">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Squareone</title>
-  <script type="module" crossorigin src="/assets/index-Cp9yhF91.js"></script>
+  <script type="module" crossorigin src="/assets/index-D-ZTA7NU.js"></script>
   <link rel="stylesheet" crossorigin href="/assets/index-BWCJCtz9.css">
 </head>
 
