cfd718ae7012a6aa50f9f1f34f14e1942987b937
Michael Ozoudeh <jesmikky@gmail.com>
2024-10-21 00:49:40 +0100

    fixed pending updates

--- resources/js/src/views/Login.vue
+++ resources/js/src/views/Login.vue
@@ -97,8 +97,8 @@ const { errors, handleSubmit, defineField, setFieldValue } = useForm({
     password: yup.string().min(6).required(),
   }),
   initialValues: {
-    email: 'jesmikky@gmail.com',
-    password: 'mikky123',
+    email: '',
+    password: '',
   },
 });
 
