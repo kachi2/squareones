5048525976d734fce553616e9ccedfaf3fe181a9
Michael Ozoudeh <jesmikky@gmail.com>
2024-12-11 03:01:12 +0100

    updated remove service method

--- app/Http/Controllers/Manage/CompanyServiceController.php
+++ app/Http/Controllers/Manage/CompanyServiceController.php
@@ -23,4 +23,22 @@ class CompanyServiceController extends Controller
         }
     }
 
+    public function StoreService($company_id, Request $req)
+    {
+        try{
+        $company = Company::where('id', $company_id)->first();
+         $regsCo = RegisteredCompany::where('company_id', $company_id)->first();
+        if($company->exists())
+        {
+            $company->update(['services' => json_encode($req->service)]);
+             $regsCo->update(['services' => json_encode($req->service)]);
+        }
+        return response()->json(['data' => $company->load('mainContact')], 200);
+        }catch(\Exception $e)
+        {
+        return response()->json(['error' => $e->getMessage()], 203);
+        }
+    }
+
+
 }
